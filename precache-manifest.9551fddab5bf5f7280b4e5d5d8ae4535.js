d42769cec50818ffca9d067ef5e24ee39f4b8806
Binod Nagarkoti <Binod1365@gmail.com>
2020-06-01 14:41:49 +0545

    Updates

---
+++ precache-manifest.9551fddab5bf5f7280b4e5d5d8ae4535.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c13a4bc940fc2de1e183e175204aaeed",
+    "url": "/covid-19/index.html"
+  },
+  {
+    "revision": "18499e3983580afc163e",
+    "url": "/covid-19/static/css/main.9245eba1.chunk.css"
+  },
+  {
+    "revision": "c177ff3ad53b76604279",
+    "url": "/covid-19/static/js/2.8b1f12ac.chunk.js"
+  },
+  {
+    "revision": "176852ae27885dfd7559e0fb4b332d24",
+    "url": "/covid-19/static/js/2.8b1f12ac.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "18499e3983580afc163e",
+    "url": "/covid-19/static/js/main.0c3df904.chunk.js"
+  },
+  {
+    "revision": "b3a3bb7da73e882ef60e",
+    "url": "/covid-19/static/js/runtime-main.aeb58cd9.js"
+  },
+  {
+    "revision": "6ce9074d039177a5b09ef64551377167",
+    "url": "/covid-19/static/media/covid-19.6ce9074d.png"
+  }
+]);

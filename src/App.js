778c83093a9140b17bc1b6fa1609504c25a5e3b9
Binod Nagarkoti <Binod1365@gmail.com>
2020-06-02 15:04:49 +0545

    adding covid-19 project and deploy

--- src/App.js
+++ src/App.js
@@ -4,47 +4,51 @@ import style from './App.module.css';
 import { fetchData } from './api';
 import coronaImage from './images/covid-19.png';
 import { setColor } from './randomColor';
+
 const Loading = require('react-loading-animation');
 
 class App extends Component {
-  constructor(props){
-    super(props)
+  constructor(props) {
+    super(props);
     this.state = {
-    data: {},
-    country: '',
-    color: '',
-  }};
+      data: {},
+      country: '',
+      color: '',
+    };
+  }
 
   async componentDidMount() {
     const fetcheddata = await fetchData();
 
     this.setState({ data: fetcheddata, color: `${setColor()}` });
-    document.body.style.backgroundColor = `${setColor()}`;
+    document.body.style.backgroundColor = `${setColor}`;
   }
+
   handleCountryChange = async (country) => {
     // console.log(country);
     const fetchedData = await fetchData(country);
     // console.log(fetchedData);
-    this.setState({ data: fetchedData, country: country });
+    this.setState({ data: fetchedData, country });
   };
+
   render() {
     const { data, country } = this.state;
     if (!data.confirmed) {
       return (
-        <div>
+        <main className={style.loading}>
           <h1 style={{ textAlign: 'center' }}>Fetching COVID-19 Data</h1>
           <Loading />
-        </div>
+        </main>
       );
     }
     return (
-      <div className={style.container}>
+      <main className={style.container}>
         <img className={style.image} src={coronaImage} alt="COVID-19" />
         <Cards data={data} />
         <CountryPicker handleCountryChange={this.handleCountryChange} />
         <Charts data={data} country={country} />
         <Footer />
-      </div>
+      </main>
     );
   }
 }
--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Typography } from '@material-ui/core';
+import { Typography, Link } from '@material-ui/core';
 import { FaFacebook, FaTwitter, FaGithub, FaLinkedin } from 'react-icons/fa';
 import { IconContext } from 'react-icons';
 import styles from './Footer.module.css';
@@ -8,32 +8,35 @@ const Footer = () => {
   return (
     <div className={styles.container}>
       <Typography>
-        {' '}
-        By <a href="https://github.com/BinodNagarkoti">Binod Nagarkoti</a>
+        By{' '}
+        <Link underline="none" color="primary" href="https://github.com/BinodNagarkoti">
+          Binod Nagarkoti
+        </Link>
       </Typography>
       <Typography className={styles.social_link}>
         <IconContext.Provider value={{ color: '#385898', size: '25px' }}>
-          <a href="https://www.facebook.com/Binodb/" name="facebook">
+          <Link underline="none" href="https://www.facebook.com/Binodb/" name="facebook">
             <FaFacebook />
-          </a>
+          </Link>
         </IconContext.Provider>
         <IconContext.Provider value={{ color: 'rgb(29, 161, 242)', size: '25px' }}>
-          <a href="https://twitter.com/binod1365" name="Twitter">
+          <Link underline="none" href="https://twitter.com/binod1365" name="Twitter">
             <FaTwitter />
-          </a>
+          </Link>
         </IconContext.Provider>
         <IconContext.Provider value={{ color: '#003f67', size: '25px' }}>
-          <a
+          <Link
+            underline="none"
             href="https://www.linkedin.com/in/binod-nagarkoti-496245128?lipi=urn%3Ali%3Apage%3Ad_flagship3_profile_view_base_contact_details%3B0rrxJXsqT0SQGAfwBMDI7A%3D%3D"
             name="LinkedIn"
           >
             <FaLinkedin />
-          </a>
+          </Link>
         </IconContext.Provider>
         <IconContext.Provider value={{ color: 'black', size: '25px' }}>
-          <a href="https://github.com/BinodNagarkoti" name="Github">
+          <Link underline="none" href="https://github.com/BinodNagarkoti" name="Github">
             <FaGithub />
-          </a>
+          </Link>
         </IconContext.Provider>
       </Typography>
       <Typography color="textSecondary"> May 31 2020 </Typography>

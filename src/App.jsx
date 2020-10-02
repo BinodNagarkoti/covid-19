53aea8fcdb24d5081a8fe7ad04de5e110ea2a223
Binod Nagarkoti <binod1365@gmail.com>
2020-10-02 15:59:48 +0545

    SEO working

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,6 @@
+/* eslint-disable jsx-a11y/click-events-have-key-events */
+/* eslint-disable jsx-a11y/no-static-element-interactions */
+/* eslint-disable jsx-a11y/anchor-is-valid */
 import React, { Component } from 'react';
 import Grid from '@material-ui/core/Grid';
 import Paper from '@material-ui/core/Paper';
